401c26715496684c89853ceac7b29d43f3e11b25
Shuping LIU <lsp.lhh@gmail.com>
2014-08-30 14:10:57 +0000

    add settings.js

--- routes/home.js
+++ routes/home.js
@@ -1,19 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var settings = require('../lib/settings');
 var postsLib = require('../lib/posts');
 
 router.get('/', function(req, res) {
     postsLib.getAll(function(posts)  {
-        var title = '高阶是对抽象的抽象';
+        var title = settings.site_title;
         posts = posts.sort(function(p1, p2) {
             return p2.id - p1.id;
         });
 	var body = {
 	    posts: posts,
 	    title: title,
-	    canonical: 'http://blog.liushuping.com/',
-	    keywords: '刘淑平,liushuping,Blog,JavaScript,C#,程序,抽象',
-	    description: '刘淑平的Blog，专注于前端程序开发，致力于提高代码质量。对JavaScript和C#以及函数式编程有浓厚兴趣，认为抽象是解决复杂软件问题的强有力工具。'
+	    canonical: settings.site_home,
+	    keywords: settings.site_keywords,
+	    description: settings.description
         };
 
         res.render('home', body);
